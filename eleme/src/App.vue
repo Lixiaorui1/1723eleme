92341bcad8807c20863f5ab494131f6c65479de7
lixiaorui123 <752839535@qq.com>
2018-01-31 22:01:24 +0800

    再次提交

--- eleme/src/App.vue
+++ eleme/src/App.vue
@@ -1,18 +1,11 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
   	<transition name="slide-fade" >
 	  	<keep-alive >
 	    	<router-view class="content"/>
 	    </keep-alive>
     </transition>
-=======
-    
-    	<keep-alive>
-      	<router-view class="content"/>
-      </keep-alive>
-    
->>>>>>> 2784a7136aa6c58dd3377327916e0b44188e2f0d
+
   </div>
 </template>
 
